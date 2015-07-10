802ef1b5be80048a0910fcfe3d78d9c3cc9953ed
Matt Neary <neary@kensho.com>
2015-07-10 03:01:07 -0400

    clean up code

--- lib/run.sh
+++ lib/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 file=${1-mimesis}
+
+# Read in the component files from tarball to a temp folder.
 mkdir -p .mimesis
 tar -xvf "$file" .mimesis &> /dev/null
+
+# Play back the terminal and audio recordings.
 play -q .mimesis/audio.aiff &> /dev/null &
 ttyplay .mimesis/rec
+
+# Kill the audio playback once the terminal session has ended.
 pkill -P $$
+
+# Clean up the recordings data file.
 rm -rf .mimesis
 
--- lib/start.sh
+++ lib/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 file=${1-mimesis}
+
+# Make a temp directory and direct audio and terminal recording to it.
 mkdir -p .mimesis
 rec .mimesis/audio.aiff &> /dev/null &
 ttyrec .mimesis/rec
+
+# End audio recording once the terminal session has ended.
 pkill -P $$
 
+# Create a tarball from the component files and tell the user.
 tar -cvf "$file" .mimesis &> /dev/null
 rm -rf .mimesis
 echo "Session recorded to $file."
